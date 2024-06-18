2417807b65618b46fec407b0c1e526e163281373
Finn <finnprice03@gmail.com>
2024-06-18 14:13:15 -0700

    base info sidebar

---
+++ public/js/highlight.js
@@ -0,0 +1,13 @@
+setHighlightInfo(0);
+
+function setHighlightInfo(buildingId) {
+    $.getJSON('/buildings.json', function(data) {
+        const building = data[buildingId];
+        $('#infoTitle').html(building.building);
+        $('#infoAddress').html(building.address);
+        $('#infoDescription').html(building.description);
+        $('#infoSecondaryInfo').html(building.secondaryInfo);
+        $('#infoBrInfo').html(building.brInfo);
+        $('.buildingImg img').attr('src', `style/images/buildings/${building.image}`);
+    });
+}
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+const chalk = require('chalk');
+const PORT = 42069;
+
+app.get('/', (req, res) => {
+    res.render('html/index');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
