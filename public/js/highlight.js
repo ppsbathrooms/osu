eb59d169da77f112331e57a0f7e31623fce0bfa8
Finn <finnprice03@gmail.com>
2024-06-26 16:45:08 -0700

    sample image color for background

--- public/js/highlight.js
+++ public/js/highlight.js
@@ -7,46 +7,53 @@ testData = {
     "brInfo": ""
 }
 
-setHighlightInfo(testData)
+// setHighlightInfo(testData)
 
 function setHighlightInfo(building) {
-    $('#infoTitle').html(building.building);
-    
-    if (building.address) {
-        $('#infoAddress').show();
-        $('#infoAddress a').attr('href', `https://www.google.com/maps/search/${building.address}, corvallis Oregon`).attr('target', '_blank').text(building.address);
-    } else {
-        $('#infoAddress').hide();
-    }
-
-    if (building.description) {
-        $('#infoDescription').show();
-        $('#infoDescription p').html(building.description);
-    } else {
-        $('#infoDescription').hide();
-    }
-
-    if (building.accessibility) {
-        $('#infoAccessibility').show();
-        $('#infoAccessibility p').html(building.accessibility);
-    } else {
-        $('#infoAccessibility').hide();
-    }
-
-    if (building.brInfo) {
-        $('#infoBrInfo').show();
-        $('#infoBrInfo p').html(building.brInfo);
-    } else {
-        $('#infoBrInfo').hide();
-    }
-
     const imgSrc = `style/images/buildings/${building.files}.jpg`;
-    $('.buildingImg img').attr('src', imgSrc).on('error', function() {
+    const $imgElement = $('.buildingImg img');
+    const $infoTitle = $('#infoTitle');
+    const $infoAddress = $('#infoAddress');
+    const $infoDescription = $('#infoDescription');
+    const $infoAccessibility = $('#infoAccessibility');
+    const $infoBrInfo = $('#infoBrInfo');
+    const $highlightInfo = $('#highlightInfo');
+
+    $imgElement.attr('src', imgSrc).on('error', function() {
         $(this).attr('src', 'style/images/buildings/no-image.jpg');
     });
-    showHighlight();
-}
 
+    $imgElement.on('load', function() {
+        if ($imgElement.attr('src') === 'style/images/buildings/no-image.jpg') {
+            $highlightInfo.css('background-color', '#333');
+        } else {
+            const color = getDominantColor(this);
+            $highlightInfo.css('background', `linear-gradient(to bottom, ${color}, ${'#121211'})`);
+        }
+
+        $infoTitle.html(building.building);
+
+        function toggleElement($element, content, isLink = false) {
+            if (content) {
+                $element.show();
+                if (isLink) {
+                    $element.find('a').attr('href', `https://www.google.com/maps/search/${content}, corvallis Oregon`).attr('target', '_blank').text(content);
+                } else {
+                    $element.find('p').html(content);
+                }
+            } else {
+                $element.hide();
+            }
+        }
+
+        toggleElement($infoAddress, building.address, true);
+        toggleElement($infoDescription, building.description);
+        toggleElement($infoAccessibility, building.accessibility);
+        toggleElement($infoBrInfo, building.brInfo);
+
+        showHighlight();
+    });
+}
 $('#info-button').on('click', () => {
     showHighlight();
 })
@@ -71,3 +78,11 @@ $(document).on('keydown', function(event) {
         hideHighlight();
     }
 });
+
+// draws image as 1px and samples color
+function getDominantColor(imageObject) {
+    ctx.drawImage(imageObject, 0, 0, 1, 1);
+    const i = ctx.getImageData(0, 0, 1, 1).data;
+    // return `rgba(${i[0]},${i[1]},${i[2]},${i[3]})`;
+    return "#" + ((1 << 24) + (i[0] << 16) + (i[1] << 8) + i[2]).toString(16).slice(1);
+}
